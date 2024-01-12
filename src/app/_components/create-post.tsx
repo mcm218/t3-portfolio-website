408139349a8bd74bd9996078177e025a0a1e4d94
Michael Muniz <mcmuniz218@gmail.com>
2024-01-11 20:10:23 -0500

    planetscale and shadcn

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -1,43 +1,82 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 import { api } from "~/trpc/react";
 
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import * as z from "zod";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { create } from "domain";
+
 export function CreatePost() {
     const router = useRouter();
-    const [name, setName] = useState("");
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
 
     const createPost = api.post.create.useMutation({
         onSuccess: () => {
             router.refresh();
-            setName("");
+            form.setValue("name", "");
         },
     });
 
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values);
+        createPost.mutate(values);
+    }
+
     return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault();
-                createPost.mutate({ name });
-            }}
-            className="flex flex-col gap-2"
-        >
-            <input
-                type="text"
-                placeholder="Title"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="w-full rounded-full px-4 py-2 text-black"
-            />
-            <button
-                type="submit"
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-                disabled={createPost.isLoading}
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col gap-2"
             >
-                {createPost.isLoading ? "Submitting..." : "Submit"}
-            </button>
-        </form>
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                    placeholder="Title"
+                                    {...field}
+                                    className="w-full rounded-full px-4 py-2 text-black"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button
+                    type="submit"
+                    className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+                    disabled={createPost.isLoading}
+                >
+                    {createPost.isLoading ? "Submitting..." : "Submit"}
+                </Button>
+            </form>
+        </Form>
     );
 }
